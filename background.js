2403363ba83ad763032bb15592312ba37b427021
zhangxu <zhangxu1573@qq.com>
2014-11-03 16:56:26 +0800

    QQ空间说说，评论抓取工具。可以统计有多少说说，评论，以及都是哪些人互动了多少次，可以看出他们的亲密度。

---
+++ background.js
@@ -0,0 +1,15 @@
+var Port;
+chrome.runtime.onConnect.addListener(function  (port) {
+	Port=port
+	console.log("background onConnect");
+	console.log("port name："+port.name)
+	port.onMessage.addListener(onMessage);
+	// body...
+});
+function onMessage (msg) {
+	var location="background";
+	var url = "infobar.html#"
+	console.log("I am in "+location);
+	console.log("I am from "+msg.hello);
+	chrome.infobars.show({tabId:Port.sender.tab.id,path: url});
+}
---
+++ contentscript.js
@@ -0,0 +1,93 @@
+var feeds=document.getElementById('host_home_feeds');
+//判断是否有我们需要分析的内容
+if (feeds) 
+{
+	var port1=chrome.runtime.connect({name:"knock"});
+//	console.log("knock was established");
+	port1.onMessage.addListener(onMessage);
+//	console.log("port1 addListener");
+	port1.postMessage({hello:"contentscript"});
+//	console.log("port1 send Message")
+	chrome.runtime.onConnect.addListener(function  (port) {
+		port2=port;
+//	console.log(port.name);
+	port2.onMessage.addListener(onMessage);
+	var location="contentscript";
+//	console.log(port.name+"come in,");
+		// body...
+	});
+}
+//消息侦听回调函数
+function onMessage (Message) {
+	//console.log(Message.hello);
+	switch (Message.act)
+	{
+		case "getPackage":
+    package=getPackage();
+    var nick=getNick(package.owner);
+	 	port2.postMessage({
+                        nick:nick,
+                        feeds:package.feeds,
+                        comments:package.comments,
+                        people:package.people,
+                        act:"tellPackage"
+                      });
+	 //	console.log("tell feeds length:"+feeds.children.length+"@@"+"@@"+nick);
+	 	break;
+	 	upload();
+	 //	console.log("@@@@@@@@@@upload");
+    break;
+    case "getMost":
+    var result=analaysis();
+    var most=[];
+    for(var i=0;i<10;i++)
+    {
+      var nick=getNick(result[result.length-i-2].qq);
+      most[i]={
+                qq:result[result.length-i-2].qq,
+                cnt:result[result.length-i-2].cnt,
+                nick:nick
+              };
+    }
+    port2.postMessage({hello:most,act:"tellMost"});
+    break;
+    
+
+	 };
+	// body...
+}
+/* 用于上传数据的暂时用不到
+function upload (argument) {
+//	console.log("@@@@@@@@in upLoad");
+	var Json=$.toJSON(shuo);
+	$.ajax({
+    url: "http://127.0.0.1:8000/fetchQzone/search",
+ 
+    // the name of the callback parameter, as specified by the YQL service
+ 	jsonp:callback,
+    // tell jQuery we're expecting JSONP
+    dataType: "jsonp",
+ 	type:"POST",
+    // tell YQL what we want and that we want JSON
+    data: {
+        shuo:Json,
+        format: "json"
+    },
+ 
+    // work with the response
+    success: function( response ) {
+    //       console.log("success"); // server response
+    },
+    error: function( xhr, status, errorThrown ) {
+        console.log( "Sorry, there was a problem!" );
+        console.log( "Error: " + errorThrown );
+        console.log( "Status: " + status );
+        console.dir( xhr );
+    },
+    complete: function( xhr, status ) {
+       console.log( "The request is complete!" );
+    }
+});
+	// body...
+}
+*/
